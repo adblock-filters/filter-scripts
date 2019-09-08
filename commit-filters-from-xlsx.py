c47108b3c080451ab0c681d6ac414331378e724e
adblock-filters <52950278+adblock-filters@users.noreply.github.com>
2019-09-08 19:30:45 +0100

    add comments

--- commit-filters-from-xlsx.py
+++ commit-filters-from-xlsx.py
@@ -1,39 +1,46 @@
 #!/usr/bin/env python3
+""" This script could be useful for retreiving filters from filters.xlsx,
+    analysing them, adding to correct easylistpolish files, 
+    auto commiting changes and creating pull-request message. 
+    
+    Run with 0-3 parameters:
+    script.py [branch-name [path_to_repository [onlyfix]]] """
 
-import datetime
-import sys
+# Import key modules
+import datetime, sys
 import pandas as pd
 from git import Repo
 
-
+# Set default location of easylistpolish repository and its files
 REPOPATH = "..\\easylistpolish"
 DIRPATH = "\\easylistpolish\\easylistpolish_"
 
+# Set default names and options
 BRANCH = "update-proposals-2"
 FILEREAD = "filters.xlsx"
 FILTERSHEET = 'filters'
 ONLYFIX = 'no'
 
 
-# correct folders
-# optional: add fix commit/filter option
-
 def setup():
+    """ If any command-line param is set, change default ones """
     if len(sys.argv) == 2:
         BRANCH = str(sys.argv[1])
     elif len(sys.argv) == 3:
-        ONLYFIX = str(sys.argv[2])
+        REPOPATH = str(sys.argv[2])
     elif len(sys.argv) == 4:
-        REPOPATH = str(sys.argv[3])
+        ONLYFIX = str(sys.argv[3])
 
 
 def open_file_to_write():
+    """ Create and set the name of the .txt file with pull message in .md format """
     file_write = "pulls\\pull-request-" + str(datetime.datetime.now())[:19].replace(":","") + ".txt"
     openfile = open(file_write, "x")
     return openfile
 
 
 def open_filter_lists():
+    """ Open particular file from easylistpolish repo """
     files = {
         'BLOCK': open(REPOPATH + DIRPATH + "specific_block.txt", "a"),
         'HIDE': open(REPOPATH + DIRPATH + "specific_hide.txt", "a"),
@@ -44,25 +51,31 @@ def open_filter_lists():
         'ADPOPUP': open(REPOPATH + DIRPATH + "adservers_popup.txt", "a"), 
         'WHITE': open(REPOPATH + DIRPATH + "whitelist.txt", "a"),
         'WHITEDIM': open(REPOPATH + DIRPATH + "whitelist_dimensions.txt", "a"), 
-        'WHITEPOPUP': open(REPOPATH + DIRPATH + "whitelist_popup.txt", "a") 
+        'WHITEPOPUP': open(REPOPATH + DIRPATH + "whitelist_popup.txt", "a"),
+        'GENBLOCK': open(REPOPATH + DIRPATH + "general_block.txt", "a"),
+        'GENHIDE': open(REPOPATH + DIRPATH + "general_hide.txt", "a"),
+        'GENPOPUP': open(REPOPATH + DIRPATH + "general_block_popup.txt", "a"), 
         }
     return files
 
 
 def write_filter_to_file(filtertype, filter):
+    """ Write filter to particular file """
     files = open_filter_lists()
     filter_file = files.get(filtertype)
     filter_file.write(filter  + "\n")
     filter_file.close()
 
 
-def open_sheet(file_path,sheetname):
+def open_sheet(file_path, sheetname):
+    """ Open and return .xls sheet """
     sheet = pd.read_excel(file_path, sheetname)
     sheet = sheet.fillna('')
     return sheet
 
 
 def domains_to_list(file_path):
+    """ Open domains list from .xls and return python list of them """
     domains_sheet = pd.read_excel(file_path, 'domains')
     domains = []
     for i in domains_sheet.index:
@@ -71,6 +84,7 @@ def domains_to_list(file_path):
 
 
 def repo_open(repo_path, branch):
+    """ Open and return git repository on selected branch """
     repo = Repo(repo_path)
     assert not repo.bare
     repo.git.checkout(branch)
@@ -78,16 +92,20 @@ def repo_open(repo_path, branch):
 
 
 def repo_commit(repo, message):
+    """ Add last changes and commit with commit message """
     repo.git.add(update=True)
     repo.git.commit('-m', message)
-    print("> commit: \t\t" + message)
+    print("> commit: \t" + message)
 
 
 def get_site_from_link(link, domains):
+    """ Cut link with domain from hyperlink """
     newlink = link[link.find('://')+3:]    
     if newlink[:3] == 'www': 
         newlink = newlink[4:]
 
+    # check if link has valid domain
+    # and return site name in site.domain format
     for domain in domains:
         result = newlink.find(domain)
         if result != -1:
@@ -96,6 +114,7 @@ def get_site_from_link(link, domains):
 
 
 def convert_to_md_link(link, notes, domains):
+    """ Return hyperlink in .md format and site name with optional notes"""
     site = get_site_from_link(link, domains)
     if notes == "":
         return "[" + link + "]("+ site + ")\n", site
@@ -104,30 +123,41 @@ def convert_to_md_link(link, notes, domains):
     
 
 def add_commit_create_pull(sheet, repo):
+    """ Check if key cells have valid content, 
+        add filters to easylistpolish files,
+        commit changes,
+        create pull-request message for all commits """
     domains = domains_to_list(FILEREAD)
     pull = open_file_to_write()
     commit_msg = ""
 
-    for i in range(5): # sheet.index:
+    # Iterate through whole sheet
+    for i in sheet.index:
         CHECK = sheet['Check'][i]
         PUSH = sheet['Push'][i]
         LINK = sheet['Link'][i]
         
+        # If filter is checked and wasn't pushed before
         if (CHECK=='yes' and PUSH=='no'):
 
+            # Set rest of the params
             NUMBER = str(sheet['No'][i])[:-2]
             FILTER = sheet['Filter'][i]
             FILTERTYPE = sheet['Type'][i]
             COMMIT = str(sheet['Commit'][i])
+
+            # If link is not empty, add another hyperlink to pull-request file
             if LINK != "":
                 MD_LINK = convert_to_md_link(LINK, sheet['Notes'][i], domains)
 
+            # If number is empty, add next hyperlink (if no filter) or next filter
             if NUMBER is "":
                 if FILTER is "":
                     pull.write(MD_LINK[0])
                 else:
                     write_filter_to_file(FILTERTYPE, FILTER)
 
+            # If number is NOT empty, commit previous changes and add next filter
             else:
                 if commit_msg != "":
                     repo_commit(repo, commit_msg)
@@ -135,27 +165,26 @@ def add_commit_create_pull(sheet, repo):
                 write_filter_to_file(FILTERTYPE, FILTER)
                 commit_msg = MD_LINK[1]
 
+                # Compose pull-request message
                 pull.write("\n")
                 pull.write("__________________________\n")
                 pull.write("**# " + NUMBER + "**\n")
                 pull.write(COMMIT + "\n")
                 pull.write(MD_LINK[0])   
 
+    # Commit of last changes
     if commit_msg != "":
         repo_commit(repo, commit_msg)
 
 
 def main():
-    print("__start")
-
+    """ Main """
+    print("> commit-filters-from-xls.py is running")
     setup()
-
     sheet = open_sheet(FILEREAD, FILTERSHEET)
     repo = repo_open(REPOPATH, BRANCH)
-
     add_commit_create_pull(sheet, repo)
-
-    print("__end")
+    print("> commit-filters-from-xls.py has finished all tasks")
 
 
 if __name__== "__main__":
